b38c68511de4ddbf6f3b1ca5de9fccfa6efc48c2
huthoinguyn <87889651+huthoinguyn@users.noreply.github.com>
2022-03-31 22:41:08 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,33 +17,46 @@ const cartNoti = `
       <h3 class="title">Add to Cart</h3>
       <p class="status">Success</p>
     </div>
-`
+`;
 const addCarts = [...document.querySelectorAll("#content .product .addtocart")];
-addCarts.forEach(item => item.addEventListener("click",handleAddtoCart));
-function handleAddtoCart(event){
-document.body.insertAdjacentHTML("afterbegin",cartNoti);
+addCarts.forEach((item) => item.addEventListener("click", handleAddtoCart));
+function handleAddtoCart(event) {
+  document.body.insertAdjacentHTML("afterbegin", cartNoti);
 }
 // Slider
 const leftIcon = document.querySelector("#slider .left__icon");
 const rightIcon = document.querySelector("#slider .right__icon");
 
-leftIcon.addEventListener("click",handleChangeImage);
-rightIcon.addEventListener("click",handleChangeImage);
+leftIcon.addEventListener("click", handlePrevImage);
+rightIcon.addEventListener("click", handleNextImage);
 const images = [
+  "./img/wk7_031322_size_1_desktop_vans.webp",
   "./img/wk03_021722_site_ads_rewards_spring_seasonal_preview_story1_us_ca_en_v1_desktop_sign-in.webp",
   "./img/wk7_031322_size_1_desktop_nikeairmax_wear_more_air.webp",
-  "./img/wk7_031322_size_1_desktop_vans.webp",
   "./img/wk9_032722_size_1_desktop_nike.webp",
   "./img/wk9_032722_size_1_desktop_vans.webp",
   "./img/wk9_032722_size_1_desktop_crocs.webp",
 ];
 const slider = document.querySelector("#slider");
-let lastImage;
-function handleChangeImage(event) {
-  const randomImage = images[Math.floor(Math.random() * images.length)];
-  if (randomImage != lastImage) {
-    slider.style.backgroundImage = `url(${randomImage})`;
-  }
-  lastImage = randomImage;
+let index = 0;
+Number(index);
+function handleChangeImage(randomImage) {
+  slider.style.backgroundImage = `url(${randomImage})`;
 }
-setInterval(handleChangeImage, 4000);
+function handleNextImage() {
+  index++;
+  if (index > images.length - 1) {
+    index = 0;
+  }
+  const randomImage = images[index];
+  handleChangeImage(randomImage);
+}
+function handlePrevImage() {
+  index--;
+  if (index < 0) {
+    index = images.length - 1;
+  }
+  const randomImage = images[index];
+  handleChangeImage(randomImage);
+}
+setInterval(handleNextImage,3000);
