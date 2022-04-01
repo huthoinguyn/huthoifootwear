e3f5eaeaac6e863862f4e47c1f25e050736c9609
huthoinguyn <87889651+huthoinguyn@users.noreply.github.com>
2022-04-01 20:54:48 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-// Cart
-const cart = document.querySelector("#cart");
-const closeIcon = document.querySelector("#cart .close__icon");
-const cartIcon = document.querySelector("#header .cart__icon");
-closeIcon.addEventListener("click", handleCloseCart);
-cartIcon.addEventListener("click", function (event) {
-  cart.style.width = "25%";
-  event.stopPropagation();
-});
-document.body.addEventListener("click", handleCloseCart);
-function handleCloseCart(event) {
-  cart.style.width = 0;
-}
-// Add to cart
-const cartNoti = `
-<div class="cart__noti">
-      <h3 class="title">Add to Cart</h3>
-      <p class="status">Success</p>
-    </div>
-`;
-const addCarts = [...document.querySelectorAll("#content .product .addtocart")];
-addCarts.forEach((item) => item.addEventListener("click", handleAddtoCart));
-function handleAddtoCart(event) {
-  document.body.insertAdjacentHTML("afterbegin", cartNoti);
-}
 // Slider
 const leftIcon = document.querySelector("#slider .left__icon");
 const rightIcon = document.querySelector("#slider .right__icon");
@@ -59,4 +34,48 @@ function handlePrevImage() {
   const randomImage = images[index];
   handleChangeImage(randomImage);
 }
-setInterval(handleNextImage,3000);
+setInterval(handleNextImage, 3000);
+// Cart
+const cart = document.querySelector("#cart");
+const closeIcon = document.querySelector("#cart .close__icon");
+const cartIcon = document.querySelector("#header .cart__icon");
+closeIcon.addEventListener("click", handleCloseCart);
+cartIcon.addEventListener("click", function (event) {
+  cart.style.width = "25%";
+  event.stopPropagation();
+});
+const subCart = document.querySelector("#cart .cart");
+document.body.addEventListener("click", handleCloseCart);
+function handleCloseCart(event) {
+  if(!event.target.contains(cart)){
+    cart.style.width = 0;
+  }
+}
+const shopping = document.querySelector("#cart .cart .shopping");
+shopping.addEventListener("click",handleCloseCart);
+// Add to cart
+const cartNoti = `
+<div class="cart__noti">
+      <h3 class="title">Add to Cart</h3>
+      <p class="status">Success</p>
+    </div>
+`;
+const addCarts = [...document.querySelectorAll("#content .product .addtocart")];
+addCarts.forEach((item) => item.addEventListener("click", handleAddtoCart));
+function handleAddtoCart(event) {
+  document.body.insertAdjacentHTML("afterbegin", cartNoti);
+}
+
+// Load
+// const loadTemplate = `
+
+
+// `;
+// window.addEventListener("load", function () {
+//   this.document.body.insertAdjacentHTML("afterbegin", loadTemplate);
+// });
+// window.addEventListener("loadeddata", function () {
+//   this.document.body.removeChild("#load");
+// });
+
+// -------
